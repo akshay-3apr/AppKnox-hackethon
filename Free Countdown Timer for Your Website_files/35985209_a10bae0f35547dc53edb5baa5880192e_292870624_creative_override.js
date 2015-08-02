0627a00bc5e80d83eca667f37ce80ce75d082486
Akshay-3apr <prakashakshay90@gmail.com>
2015-08-02 10:36:09 +0530

    First Commit

---
+++ Free Countdown Timer for Your Website_files/35985209_a10bae0f35547dc53edb5baa5880192e_292870624_creative_override.js
@@ -0,0 +1,110 @@
+(function() {
+  var creativeDefinition = {
+    customScriptUrl: '',
+    isDynamic: false,
+    delayedImpression: false,
+    standardEventIds: {
+      DISPLAY_TIMER: '2',
+      INTERACTION_TIMER: '3',
+      INTERACTIVE_IMPRESSION: '4',
+      FULL_SCREEN_VIDEO_PLAYS: '5',
+      FULL_SCREEN_VIDEO_COMPLETES: '6',
+      FULL_SCREEN_AVERAGE_VIEW_TIME: '7',
+      MANUAL_CLOSE: '8',
+      BACKUP_IMAGE_IMPRESSION: '9',
+      EXPAND_TIMER: '10',
+      VIDEO_PLAY: '11',
+      VIDEO_VIEW_TIMER: '12',
+      VIDEO_COMPLETE: '13',
+      VIDEO_INTERACTION: '14',
+      VIDEO_PAUSE: '15',
+      VIDEO_MUTE: '16',
+      VIDEO_REPLAY: '17',
+      VIDEO_MIDPOINT: '18',
+      FULL_SCREEN_VIDEO: '19',
+      VIDEO_STOP: '20',
+      VIDEO_FIRST_QUARTILE: '960584',
+      VIDEO_THIRD_QUARTILE: '960585',
+      VIDEO_UNMUTE: '149645',
+      FULL_SCREEN: '286263',
+      DYNAMIC_CREATIVE_IMPRESSION: '536393',
+      HTML5_CREATIVE_IMPRESSION: '871060'
+    },
+    exitEvents: [
+      {
+        name: 'exitBtn',
+        reportingId: '2242862',
+        url: 'https://www.google.com/intx/en_in/work/apps/business/?utm_source\x3dgdn\x26utm_medium\x3ddisplay\x26utm_campaign\x3djapac-in-en-apps-dco-newbiz-trial-%epid!\x26utm_content\x3dbanner-728x90-gizmo1-newbiz-%ecid!',
+        targetWindow: '_blank',
+        windowProperties: ''
+      }
+    ],
+    timerEvents: [
+    ],
+    counterEvents: [
+    ],
+    childFiles: [
+      {
+        name: 'GAFW_Gmail_Birch_IN_728x90.jpg',
+        url: '/ads/richmedia/studio/pv2/35983945/20150312172249277/GAFW_Gmail_Birch_IN_728x90.jpg',
+        isVideo: false,
+        transcodeInformation: null
+      }
+    ],
+    videoFiles: [
+    ],
+    videoEntries: [
+    ],
+    primaryAssets: [
+      {
+        id: '35983813',
+        artworkType: 'FLASH',
+        displayType: 'BANNER',
+        width: '728',
+        height: '90',
+        servingPath: '/ads/richmedia/studio/pv2/35983945/20150312172249277/GAFW_Gmail_Gizmo_EN-IN_728x90.swf',
+        zIndex: '1000000',
+        customCss: '',
+        flashArtworkTypeData: {
+          actionscriptVersion: '3',
+          wmode: 'opaque',
+          sdkVersion: '2.4.4',
+          flashBackgroundColor: '',
+          allowScriptAccess: 'always'
+        },
+        htmlArtworkTypeData: null,
+        floatingDisplayTypeData: null,
+        expandingDisplayTypeData: null,
+        imageGalleryTypeData: null,
+        pageSettings:null,
+layoutsConfig: null,
+layoutsApi: null
+      }
+    ]
+  }
+  var rendererDisplayType = '';
+  rendererDisplayType += 'flash_';
+  var rendererFormat = 'inpage';
+  var rendererName = rendererDisplayType + rendererFormat;
+
+  var creativeId = '62110214';
+  var adId = '292870624';
+  var templateVersion = '200_89';
+  var studioObjects = window['studioV2'] = window['studioV2'] || {};
+  var creativeObjects = studioObjects['creatives'] = studioObjects['creatives'] || {};
+  var creativeKey = [creativeId, adId].join('_');
+  var creative = creativeObjects[creativeKey] = creativeObjects[creativeKey] || {};
+  creative['creativeDefinition'] = creativeDefinition;
+  var adResponses = creative['adResponses'] || [];
+  for (var i = 0; i < adResponses.length; i++) {
+    adResponses[i].creativeDto && adResponses[i].creativeDto.csiEvents &&
+        (adResponses[i].creativeDto.csiEvents['pe'] =
+            adResponses[i].creativeDto.csiEvents['pe'] || (+new Date));
+  }
+  var loadedLibraries = studioObjects['loadedLibraries'] = studioObjects['loadedLibraries'] || {};
+  var versionedLibrary = loadedLibraries[templateVersion] = loadedLibraries[templateVersion] || {};
+  var typedLibrary = versionedLibrary[rendererName] = versionedLibrary[rendererName] || {};
+  if (typedLibrary['bootstrap']) {
+    typedLibrary.bootstrap();
+  }
+})();
